442aab2e878d47fb0e906fd64027e53991d6cfa9
Ramanjs <raman.s2662@gmail.com>
2020-06-06 18:19:15 +0530

    testing

--- insert.php
+++ insert.php
@@ -1,37 +1,40 @@
 <?php
 
+try {
+	//Create connection
+	$db = parse_url(getenv("DATABASE_URL"));
 
-//Create connection
-$db = parse_url(getenv("DATABASE_URL"));
-
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$pdo = new PDO("pgsql:" . sprintf(
+	    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	    $db["host"],
+	    $db["port"],
+	    $db["user"],
+	    $db["pass"],
+	    ltrim($db["path"], "/")
+	));
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	
-	 
-	$stmt = $pdo->prepare("INSERT INTO STUDENTS (NAME, CLASS, SECTION) VALUES(:name, :class, :section);");
-	$stmt->bindParam(':name', $name);
-		$stmt->bindParam(':class', $class);
-		$stmt->bindParam(':section', $section);
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		 
+		$stmt = $pdo->prepare("INSERT INTO STUDENTS (NAME, CLASS, SECTION) VALUES(:name, :class, :section);");
+		$stmt->bindParam(':name', $name);
+			$stmt->bindParam(':class', $class);
+			$stmt->bindParam(':section', $section);
 
-		$name = test_input($_POST["name"]);
-	$class = test_input($_POST["class"]);
-	$section = test_input($_POST["section"]);
-	$stmt->execute();
-	
-	
-	echo "New record created <br>";
+			$name = test_input($_POST["name"]);
+		$class = test_input($_POST["class"]);
+		$section = test_input($_POST["section"]);
+		$stmt->execute();
+		
+		
+		echo "New record created <br>";
 
+	}
+} catch (Exception $e) {
+	echo "error :".$e->getMessage();
 }
 
 function test_input($data) {
