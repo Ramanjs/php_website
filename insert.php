5ee3bcc3bf0c53cf1129cc2fafd2e55e18c825c1
Ramanjs <raman.s2662@gmail.com>
2020-06-06 17:59:54 +0530

    cleaned comments

--- insert.php
+++ insert.php
@@ -1,6 +1,6 @@
 <?php
 
-// try{
+try{
 	//Create connection
 	$db = parse_url(getenv("DATABASE_URL"));
 
@@ -42,36 +42,10 @@
 	}
 
 
-// }catch(PDOException $e){
-// 	echo "connection error :" . $e->getMessage();
-// }
-// //Check connection
-// if($conn->connect_error){
-// 	die("Connection failed: ".$conn->connect_error);
-// }
-// // echo "Connected successfully <br>";
+}catch(PDOException $e){
+	echo "connection error :" . $e->getMessage();
+}
 
-// // use database
-// $sql = "USE myDb;";
-// if($conn->query($sql) !== TRUE){
-// 	echo "Error creating database: ".$conn->error;
-// }
-
-// $name = $class = $section = "";
-
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-// 	$name = test_input($_POST["name"]);
-// 	$class = test_input($_POST["class"]);
-// 	$section = test_input($_POST["section"]);
-	 
-// 	$sql = "INSERT INTO STUDENT (NAME,CLASS,SECTION) VALUES('$name', '$class', '$section');";
-	
-// 	if($conn->query($sql) === TRUE){
-// 		echo "New record created <br>";
-// 	}else{
-// 		echo "error: ".$conn->error."<br>";
-// 	}
-// }
 $pdo = null;
 
 ?>
