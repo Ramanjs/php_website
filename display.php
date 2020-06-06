0dff11394869fce30f7d606a5fa56d92922204fc
Ramanjs <raman.s2662@gmail.com>
2020-06-06 17:10:07 +0530

    display

--- display.php
+++ display.php
@@ -1,6 +1,6 @@
 <?php
 echo "<table style='border: solid 1px black;'>";
-echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+echo "<tr><th>Id</th><th>name</th><th>class</th><th>section</th></tr>";
 
 class TableRows extends RecursiveIteratorIterator {
   function __construct($it) {
