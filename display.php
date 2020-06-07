7e0eee5ba597834256342371e18e419231a9f054
Ramanjs <raman.s2662@gmail.com>
2020-06-07 18:13:26 +0530

    view port

--- display.php
+++ display.php
@@ -1,5 +1,4 @@
 <?php
-echo "<meta name="viewport" content="width=device-width, initial-scale=1.0"/>";
 echo "<table style='border: solid 1px black;'>";
 echo "<tr><th>Id</th><th>Name</th><th>Class</th><th>Section</th></tr>";
 
